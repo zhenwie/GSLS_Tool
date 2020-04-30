10e99a18f5f2791be1f6cab1543913b9bb00d4cd
1079374315 <PlayfulKing1079>
2020-04-30 11:48:08 +0800

    Initial commit GSLS_Tool

---
+++ app/src/main/java/com/gsls/gtlibrary/activity/AndroidActivity.java
@@ -0,0 +1,17 @@
+package com.gsls.gtlibrary.activity;
+
+import android.os.Bundle;
+
+import com.gsls.gtlibrary.R;
+import com.gsls.gtlibrary.util.GT;
+
+@GT.Annotations.GT_Activity(R.layout.activity_main)
+public class AndroidActivity extends GT.AnnotationActivity {
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        build(this);//绑定 Activity
+
+    }
+
+}
