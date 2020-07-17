ba918f3a18f4d97137990410ce027545ec287e5e
1079374315 <PlayfulKing1079>
2020-07-17 15:08:01 +0800

    * 更新时间:2020.7.17  * <p> CSDN 详细教程:https://blog.csdn.net/qq_39799899/article/details/98891256  * <p> CSDN 博客:https://blog.csdn.net/qq_39799899  * 更新内容：（1.1.8 版本 GT_Fragment 重构代码 增加启动模式 与 切换方式）  * 1.更新了 HttpUtil (网络请求)类  * 2.更新了 GT_Fragment 类 增加了页面数据恢复 与 BaseFragments 的优化（BaseFragment 增加了 onBackPressed 方法）  * 3.增加了 logAll 与 errAll 增加打印所有日志方法  * <p> CSDN 详细教程:https://blog.csdn.net/qq_39799899/article/details/98891256  * <p> CSDN 博客:https://blog.csdn.net/qq_39799899

--- app/src/main/java/com/gsls/gtlibrary/activity/AndroidActivity.java
+++ app/src/main/java/com/gsls/gtlibrary/activity/AndroidActivity.java
@@ -8,9 +8,6 @@ import com.gsls.gtlibrary.util.GT;
 @GT.Annotations.GT_Activity(R.layout.activity_main)
 public class AndroidActivity extends GT.AnnotationActivity {
 
-    @GT.Hibernate.Build
-    private GT.Hibernate hibernate;
-
     @Override
     protected void initView(Bundle savedInstanceState) {
         build(this);//绑定 Activity
