3060eaac7b01ceede593d4c7926adad3e21caad8
1079374315 <1079374315@qq.com>
2022-03-25 21:09:32 +0800

    * 更新时间:2021.3.25  * CSDN 博客/官网教程:https://blog.csdn.net/qq_39799899  * GitHub https://github.com/1079374315/GT  * 更新内容：（1.3.6.6 版本 大爆料：新增 MVC、MVP、MVVM 辅助框架）  * 内容如下：  * 1.新增 GT_PopupWindow 类  * 2.LOG 类新增 GT.logt("标记日志"); GT.errt("标记日志");  * 3.优化 时间与时间戳互转的方法 GT.GT_Date.timeToTimestampToTime(String timeOrTimestamp, String timeFormat)  * 4.新增 编程辅助框架 MVC/MVP/MVVM/GT/GT_Binding模式  * 5.增强 AnnotationAssist 反射、注解工具类,新增以下功能方法  * 1.获取类的泛类型  * 2.反射类中方法进行赋值  * 3.获取反射方法的返回值  * 4.获取反射变量的值(可私用变量)  * 5.利用反射给变量设置值(可私有变量)  * 6.反射该类所有的变量值 (可接受类型 class、String(类包名)、实体类)  * 7.字符串转Class  * 6.增强分享功能  * 7.优化所有的 DataBinding自动生成类 ，增强 GT_BindingViewModel 并支持 通过泛类映射实例化 ，增加了 适配器的 DataBinding  * 8.增加 WebViewUtils 类

---
+++ app/src/main/java/com/gsls/gtlibrary/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gsls.gtlibrary;
+
+import android.os.Bundle;
+
+import com.gsls.gt.GT;
+
+@GT.Annotations.GT_AnnotationActivity(R.layout.activity_main)
+public class MainActivity extends GT.GT_Activity.AnnotationActivity {
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        super.initView(savedInstanceState);
+
+    }
+
+}
